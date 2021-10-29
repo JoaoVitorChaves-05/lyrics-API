aaad649ce94b0a58a4cae9e9e9445c9bfbf16b03
João Vitor Mancio Chaves <joaovmanciochaves@gmail.com>
2021-10-29 16:15:39 -0300

    Tratamento erro da API

--- app.js
+++ app.js
@@ -6,24 +6,35 @@ const puppeteer = require('puppeteer');
 const PORT = process.env.PORT || 3000
 
 const getLyrics = async (music, res) => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ["--no-sandbox"]
-    });
-    const page = await browser.newPage();
-    await page.goto(`https://www.letras.mus.br/?q=${music}`);
-    await page.waitForSelector('#___gcse_0 > div > div > div > div.gsc-wrapper > div.gsc-resultsbox-visible > div > div > div.gsc-expansionArea > div:nth-child(1) > div.gs-webResult.gs-result > div.gsc-thumbnail-inside > div > a')
-    await page.click("#___gcse_0 > div > div > div > div.gsc-wrapper > div.gsc-resultsbox-visible > div > div > div.gsc-expansionArea > div:nth-child(1) > div.gs-webResult.gs-result > div.gsc-thumbnail-inside > div > a")
-    
-    await page.waitForSelector("#js-lyric-cnt > article > div.cnt-letra-trad.g-pr.g-sp > div.cnt-letra.p402_premium")
-    const nodes = await page.evaluate(() => {
-        const elements = document.getElementsByClassName('cnt-letra p402_premium')
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ["--no-sandbox"]
+        });
+
+        const page = await browser.newPage();
+
+        await page.goto(`https://www.letras.mus.br/?q=${music}`);
+        await page.waitForSelector('#___gcse_0 > div > div > div > div.gsc-wrapper > div.gsc-resultsbox-visible > div > div > div.gsc-expansionArea > div:nth-child(1) > div.gs-webResult.gs-result > div.gsc-thumbnail-inside > div > a', {timeout: 5000})
+        await page.click("#___gcse_0 > div > div > div > div.gsc-wrapper > div.gsc-resultsbox-visible > div > div > div.gsc-expansionArea > div:nth-child(1) > div.gs-webResult.gs-result > div.gsc-thumbnail-inside > div > a")
         
-        return elements[0].outerText
-    })
-    console.log(nodes)
-    browser.close()
-    await res.json({song: nodes})
+        await page.waitForSelector("#js-lyric-cnt > article > div.cnt-letra-trad.g-pr.g-sp > div.cnt-letra.p402_premium", {timeout: 5000})
+        
+        const nodes = await page.evaluate(() => {
+            const elements = document.getElementsByClassName('cnt-letra p402_premium')
+            
+            return elements[0].outerText
+        })
+
+        console.log(nodes)
+
+        browser.close()
+
+        await res.json({song: nodes})
+    } catch (err) {
+        await res.json({song: "Song not found."})
+    }
+    
 }
 
 app.get('/', (req, res) => {
